c65c5c7f40349e0e85803e83c8f8478e365b233d
John Connolly <connoljo2@gmail.com>
2019-03-12 16:58:25 -0300

    Added more stuff

--- djangoapp/weather.py
+++ djangoapp/weather.py
@@ -5,12 +5,19 @@ class Weather(object):
     def __init__(self, data):
         self.data = data
         self.kelvin = data['main']['temp']
-
-    def decode(self): 
-        print(self.data['main']['temp'])
+        self.wind = int(data['wind']['speed'])
+        self.clouds = int(data['clouds']['all'])
 
     def celcius(self): 
         return int(self.kelvin - 273.15)
+        
     def jsonResp(self):
         c = self.celcius()
-        return json.dumps({ "temp": c })
+        return json.dumps({ "temp": c , "slogan" : self.generateSlogan() })
+
+    def generateSlogan(self):
+        if (self.wind > 20):
+            return "It's a windy day!"
+        elif (self.clouds > 70):
+            return "It's cloudy!"
+        return "It's a nice day"
